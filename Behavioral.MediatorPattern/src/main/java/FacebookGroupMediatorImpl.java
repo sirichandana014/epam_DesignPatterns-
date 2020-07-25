086461e77e5aaf223cbdfe0c84a68a8d81a55677
sirichandana014 <60822411+sirichandana014@users.noreply.github.com>
2020-07-25 17:32:17 +0530

    Add files via upload

---
+++ Behavioral.MediatorPattern/src/main/java/FacebookGroupMediatorImpl.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FacebookGroupMediatorImpl implements FacebookGroupMediator
+{
+
+    private List<User> users;
+
+    public FacebookGroupMediatorImpl()
+    {
+        this.users = new ArrayList<User>();
+    }
+
+    public void registerUser( User user )
+    {
+        this.users.add(user);
+    }
+
+    public void sendMessage( String msg, User user )
+    {
+        for( User u : this.users )
+        {
+            // message should not be received by the user sending it
+            if( u != user )
+            {
+                u.receive(msg);
+            }
+        }
+    }
+
+}
