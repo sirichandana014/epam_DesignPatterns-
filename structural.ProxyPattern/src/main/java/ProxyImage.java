b52d2a430b2636c5d26a786c799757599d0021ef
sirichandana014 <60822411+sirichandana014@users.noreply.github.com>
2020-07-25 13:34:57 +0530

    Add files via upload

---
+++ structural.ProxyPattern/src/main/java/ProxyImage.java
@@ -0,0 +1,29 @@
+class ProxyImage implements Image
+{
+
+    private RealImage image    = null;
+    private String    filename = null;
+
+    /**
+     * Constructor
+     *
+     * @param FILENAME
+     */
+    public ProxyImage( final String FILENAME )
+    {
+        filename = FILENAME;
+    }
+
+    /**
+     * Displays the image
+     */
+    public void displayImage()
+    {
+        if( image == null )
+        {
+            image = new RealImage(filename);
+        }
+        image.displayImage();
+    }
+
+}
