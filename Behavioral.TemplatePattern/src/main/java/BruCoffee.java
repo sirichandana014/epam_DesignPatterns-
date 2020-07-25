35a48175df617d0a1c73e9f0737e34bd559a9b92
sirichandana014 <60822411+sirichandana014@users.noreply.github.com>
2020-07-25 18:29:16 +0530

    Add files via upload

---
+++ Behavioral.TemplatePattern/src/main/java/BruCoffee.java
@@ -0,0 +1,28 @@
+public class BruCoffee extends CoffeeTemplate
+{
+
+    public void boilWater()
+    {
+        System.out.println("100 ml water boiled");
+
+    }
+
+    public void addMilk()
+    {
+        System.out.println("100 ml milk Added");
+
+    }
+
+    public void addSugar()
+    {
+        System.out.println("100 grams sugar Added");
+
+    }
+
+    public void addCoffeePowder()
+    {
+        System.out.println("10 grams Bru coffee powder Added");
+
+    }
+
+}
