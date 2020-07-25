e88f1a3b6aa840842c385d26cb908eda0a55366a
sirichandana014 <60822411+sirichandana014@users.noreply.github.com>
2020-07-25 18:39:12 +0530

    Add files via upload

---
+++ Behavioral.VisitorPattern/src/main/java/Child.java
@@ -0,0 +1,34 @@
+public class Child implements Visitable
+{
+
+    private String name;
+
+    private String healthStatus;
+
+    public Child( String name )
+    {
+        super();
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getHealthStatus()
+    {
+        return healthStatus;
+    }
+
+    public void setHealthStatus( String healthStatus )
+    {
+        this.healthStatus = healthStatus;
+    }
+
+    public void accept( Visitor visitor )
+    {
+        visitor.visit(this);
+    }
+
+}
